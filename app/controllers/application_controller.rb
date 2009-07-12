aff6a66cc982d7e98a2e81e25b3a952ef0893c07
John Grimes <John.Grimes@blavel.com>
2009-07-12 22:41:09 +0100

    Making small user interface tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   filter_parameter_logging :password
 
+  #TODO: Customise error pages
+
   protected
 
   def current_user_session
