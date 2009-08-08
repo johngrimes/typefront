bfa5521c4c33dab532784d51624f60377da95539
John Grimes <me@johngrimes.info>
2009-08-08 19:26:29 +0300

    Adding google analytics

---
+++ public/javascripts/common/analytics.js
@@ -0,0 +1,6 @@
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+try {
+  var pageTracker = _gat._getTracker("UA-3880720-4");
+  pageTracker._trackPageview();
+} catch(err) {}
