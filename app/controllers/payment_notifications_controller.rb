bf842fa487c7251e76ba1abc1eb470010485f09b
John Grimes <me@johngrimes.info>
2009-12-04 08:48:29 +1000

    Adding create payment notification action

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,5 +1,12 @@
 class PaymentNotificationsController < ApplicationController
-  def create
-  end
+  protect_from_forgery :except => [ :create ]
 
+  def create
+    PaymentNotification.create!(:params => params,
+                                :user_id => params[:custom],
+                                :type => params[:txn_type],
+                                :status => params[:payment_status],
+                                :transaction_id => params[:txn_id])
+    render :nothing => true
+  end
 end
