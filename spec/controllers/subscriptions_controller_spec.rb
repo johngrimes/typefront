57f54540d862d10cc56c0ea9298cc7c1ee8f1df5
John Grimes <me@johngrimes.info>
2009-12-05 20:57:39 +1000

    Generating subscriptions controller

---
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe SubscriptionsController do
+
+  #Delete this example and add some real ones
+  it "should use SubscriptionsController" do
+    controller.should be_an_instance_of(SubscriptionsController)
+  end
+
+end
