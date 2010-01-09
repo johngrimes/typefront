0897bc4081b233e2a2f786981d26da7b6817a4de
John Grimes <me@johngrimes.info>
2010-01-09 16:13:55 +1000

    Adding spec for ProcessBillingJob

---
+++ spec/lib/process_billing_job_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ProcessBillingJob do
+  it 'should perform job' do
+    @user = mock()
+    User.expects(:find).with(1).returns(@user)
+    @user.expects(:process_billing)
+    job = ProcessBillingJob.new(1)
+    job.perform
+  end
+
+  it 'should send admin mail on error' do
+    User.expects(:find).with(1).returns(nil)
+    AdminMailer.expects(:deliver_billing_job_error)
+    job = ProcessBillingJob.new(1)
+    doing {
+      job.perform
+    }.should raise_error
+  end
+end
