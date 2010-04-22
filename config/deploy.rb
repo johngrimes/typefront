28600a8e29807af622e8873fd9d3036e741f90a7
John Grimes <me@johngrimes.info>
2010-04-22 20:13:43 +1000

    Refactoring deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,9 +24,16 @@ task :to_prod do
   environment = 'production'
 end
 
-# Remove all but 5 deployed releases after each deployment
-after :deploy, 'set_permissions'
-after :deploy, 'deploy:cleanup'
+after 'deploy:setup', 'typefront:set_permissions'
+
+after 'deploy:update_code', 
+  'typefront:set_permissions',
+  'typefront:create_symlinks',
+  'typefront:create_failed_fonts',
+  'typefront:compile_styles',
+  'typefront:run_tests'
+
+after 'deploy', 'deploy:cleanup'
 
 namespace :deploy do
   task :restart do
@@ -35,39 +42,41 @@ namespace :deploy do
   end
 end
 
-task :after_update_code, :roles => :web do
-  set_permissions
+namespace :typefront do
+  task :create_symlinks, :roles => :web do
+    # Create symbolic link to a common database.yml file in the shared directory,
+    # which is not under source control
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 
-  # Create symbolic link to a common database.yml file in the shared directory,
-  # which is not under source control
-  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    # Symlink to rake task for controlling thin cluster
+    run "ln -nfs #{deploy_to}/../common/tasks/thin.rake #{release_path}/lib/tasks/thin.rake"
+  end
 
-  # Make sure failed fonts directory exists
-  run "mkdir -p #{release_path}/public/system/failed_fonts/test"
+  task :create_failed_fonts, :roles => :web do
+    # Make sure failed fonts directory exists
+    run "mkdir -p #{release_path}/public/system/failed_fonts/test"
+  end
 
-  # Symlink to rake task for controlling thin cluster
-  run "ln -nfs #{deploy_to}/../common/tasks/thin.rake #{release_path}/lib/tasks/thin.rake"
+  task :compile_styles, :roles => :web do
+    # Compile Compass stylesheets
+    run "cd #{release_path} && compass -u"
+  end
 
-  # Make sure gems and database schema are up to date
-  run "cd #{release_path}; rake gems:build"
-  run "cd #{release_path}; rake db:migrate RAILS_ENV=development"
-  run "cd #{release_path}; rake db:seed RAILS_ENV=development"
-  run "cd #{release_path}; rake db:migrate RAILS_ENV=#{environment}"
-  run "cd #{release_path}; rake db:seed RAILS_ENV=#{environment}"
+  task :run_tests, :roles => :web do
+    # Make sure gems and database schema are up to date
+    run "cd #{release_path} && rake gems:build"
+    run "cd #{release_path} && rake db:migrate RAILS_ENV=development"
+    run "cd #{release_path} && rake db:seed RAILS_ENV=development"
+    run "cd #{release_path} && rake db:migrate RAILS_ENV=#{environment}"
+    run "cd #{release_path} && rake db:seed RAILS_ENV=#{environment}"
 
-  # Compile Compass stylesheets
-  run "cd #{release_path}; compass -u"
+    # Make sure all tests pass
+    run "cd #{release_path} && rake db:test:prepare"
+    run "cd #{release_path} && rake spec"
+  end
 
-  # Make sure all tests pass
-  run "cd #{release_path}; rake db:test:prepare"
-  run "cd #{release_path}; rake spec"
-end
-
-task :after_setup, :roles => :web do
-  set_permissions
-end
-
-task :set_permissions do
-  sudo "chown -R deploy:www-data #{deploy_to}"
-  sudo "chmod -R g+w #{deploy_to}"
+  task :set_permissions do
+    sudo "chown -R deploy:www-data #{deploy_to}"
+    sudo "chmod -R g+w #{deploy_to}"
+  end
 end
