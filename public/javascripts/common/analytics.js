05fc2c05b7607b97c69e5caf022fe9e6565d8e1b
John Grimes <me@johngrimes.info>
2010-06-12 18:36:40 +1000

    Adding cross-domain tracking for Google Analytics
    To solve problem of https pages not being tracked

--- public/javascripts/common/analytics.js
+++ public/javascripts/common/analytics.js
@@ -2,5 +2,15 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 try {
   var pageTracker = _gat._getTracker("UA-3880720-4");
+  pageTracker._setDomainName("none");
+  pageTracker._setAllowLinker(true);
   pageTracker._trackPageview();
 } catch(err) {}
+$('a').each(function() {
+  if ($(this).attr('href').indexOf('https') > 0) {
+    $(this).click(function() {
+      pageTracker._link(this.href);
+      return false;
+    });
+  });
+});
