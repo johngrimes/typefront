16c0d8dffbeec71e4b17a2273699800e11510fbc
John Grimes <me@johngrimes.info>
2010-06-15 18:02:28 +1000

    Revert "Adding bundler"
    This reverts commit 432bf5b601740582c80a72d21c698db58165f5d6.

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,38 @@
 require 'rubygems'
 gem 'soap4r'
 
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "rspec", 
+    :lib => false, 
+    :version => "~> 1.2.9"
+  config.gem "rspec-rails", 
+    :lib => false, 
+    :version => "~> 1.2.9"
+  config.gem 'mocha'
+  config.gem 'authlogic'
+  config.gem "thoughtbot-factory_girl", 
+    :lib => "factory_girl", 
+    :source => "http://gems.github.com"
+  config.gem 'mislav-will_paginate', 
+    :version => '~> 2.3.8', 
+    :lib => 'will_paginate', 
+    :source => 'http://gems.github.com'
+  config.gem 'haml',
+    :version => '2.2.13'
+  config.gem 'chriseppstein-compass',
+    :lib => 'compass',
+    :version => '0.8.17'
+  config.gem 'chriseppstein-compass-960-plugin',
+    :lib => 'ninesixty',
+    :version => '0.9.7'
+  config.gem 'smurf', 
+    :lib => 'smurf', 
+    :source => 'http://gemcutter.org'
+
   config.action_controller.session_store = :active_record_store
 
   config.time_zone = 'UTC'
