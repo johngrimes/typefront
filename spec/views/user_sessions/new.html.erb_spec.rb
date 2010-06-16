8a76c9dd29e82ec7379fce94a0768fff7dd23168
John Grimes <me@johngrimes.info>
2010-06-16 10:41:41 +1000

    Updating view spec for user sessions

--- spec/views/user_sessions/new.html.erb_spec.rb
+++ spec/views/user_sessions/new.html.erb_spec.rb
@@ -1,13 +1,13 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'spec_helper'
 
-describe "/user_sessions/new" do
-  before(:each) do
+describe 'user_sessions/new.html.erb' do
+  before do
     activate_authlogic
     assigns[:session] = UserSession.new
-    render 'user_sessions/new', :layout => 'standard'
   end
 
-  it 'should spit out valid XHTML' do
-    response.should be_valid_xhtml
+  it 'should render successfully' do
+    render 'user_sessions/new', :layout => 'standard'
+    response.should be_success
   end
 end
