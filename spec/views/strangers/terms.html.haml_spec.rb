30540222e6688819e2bdf7c6ce37f27c9d09b5ac
John Grimes <me@johngrimes.info>
2010-06-18 08:12:36 +1000

    Refactoring strangers/terms into HAML/Markdown

--- spec/views/strangers/terms.html.haml_spec.rb
+++ spec/views/strangers/terms.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'strangers/terms.html.erb' do
+describe 'strangers/terms.html.haml' do
   it 'should render successfully' do
     render 'strangers/terms', :layout => 'blank'
     response.should be_success
