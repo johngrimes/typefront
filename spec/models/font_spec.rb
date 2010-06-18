075e10807ee486013b33de0b926b92f5d546f33f
John Grimes <me@johngrimes.info>
2010-06-18 12:55:34 +1000

    Refactoring spec for font model

--- spec/models/font_spec.rb
+++ spec/models/font_spec.rb
@@ -1,10 +1,8 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe Font do
-  fixtures :all
-
-  describe 'attributes' do
-    before(:each) do
+  describe 'validation' do
+    before do
       @valid = Factory.build(:font)
     end
 
@@ -27,58 +25,66 @@ describe Font do
     end
   end
 
-  it 'should log a request successfully' do
-    LoggedRequest.any_instance.expects(:save)
-    fonts(:duality).log_request('show',
-      :remote_ip => '193.485.378.485',
-      :referer => 'http://johnsmith.com/index.php',
-      :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+  describe 'log_request' do
+    it 'should be successful' do
+      LoggedRequest.any_instance.expects(:save)
+      fonts(:duality).log_request('show',
+        :remote_ip => '193.485.378.485',
+        :referer => 'http://johnsmith.com/index.php',
+        :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+    end
+
+    it 'should be successful with a blank referer' do
+      LoggedRequest.any_instance.expects(:save)
+      fonts(:duality).log_request('show',
+        :remote_ip => '193.485.378.485',
+        :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+    end
+
+    it 'should not log a request with a TypeFront referer' do
+      LoggedRequest.any_instance.expects(:save).never
+      fonts(:duality).log_request('show',
+        :remote_ip => '193.485.378.485',
+        :referer => "#{$HOST}/fonts/15",
+        :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+    end
   end
 
-  it 'should log a request with a blank referer' do
-    LoggedRequest.any_instance.expects(:save)
-    fonts(:duality).log_request('show',
-      :remote_ip => '193.485.378.485',
-      :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+  describe 'full_name' do
+    it 'should be successful' do
+      fonts(:doradani_regular).full_name.should == 'Doradani Regular'
+    end
+
+    it 'should return family name when subfamily is blank' do
+      font = fonts(:doradani_regular)
+      font.font_subfamily = nil
+      font.full_name.should == 'Doradani'
+    end
   end
 
-  it 'should not log a request with a TypeFront referer' do
-    LoggedRequest.any_instance.expects(:save).never
-    fonts(:duality).log_request('show',
-      :remote_ip => '193.485.378.485',
-      :referer => "#{$HOST}/fonts/15",
-      :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5')
+  describe 'format' do
+    it 'should raise an error if retrieving a missing format with the raise error option' do
+      doing {
+        fonts(:duality).format(:pfb, :raise_error => true)
+      }.should raise_error(ActiveRecord::RecordNotFound, 'Could not find the specified format for that font.')
+    end
+
+    it 'should not raise an error if retrieving a missing format without the raise error option' do
+      fonts(:duality).format(:pfb)
+    end
   end
 
-  it 'should return full name when subfamily is blank' do
-    font = fonts(:doradani_regular)
-    font.font_subfamily = nil
-    font.full_name.should == 'Doradani'
-  end
-
-  it 'should return full name when both family and subfamily are present' do
-    fonts(:doradani_regular).full_name.should == 'Doradani Regular'
-  end
-
-  it 'should raise an error if retrieving a missing format with the raise error option' do
-    doing {
-      fonts(:duality).format(:pfb, :raise_error => true)
-    }.should raise_error(ActiveRecord::RecordNotFound, 'Could not find the specified format for that font.')
-  end
-
-  it 'should not raise an error if retrieving a missing format without the raise error option' do
-    fonts(:duality).format(:pfb)
-  end
-
-  it 'should create and generate new formats successfully' do
-    FontAdapter.any_instance.expects(:to_ttf)
-    FontAdapter.any_instance.expects(:to_otf)
-    FontAdapter.any_instance.expects(:to_woff)
-    FontAdapter.any_instance.expects(:to_eot)
-    FontAdapter.any_instance.expects(:to_svg)
-    ActionController::TestUploadedFile.expects(:new).times(5)
-    Format.any_instance.expects(:save!).times(5)
-    FileUtils.expects(:rm).times(5)
-    font = Factory.create(:font)
+  describe 'post_process' do
+    it 'should create and generate new formats successfully' do
+      FontAdapter.any_instance.expects(:to_ttf)
+      FontAdapter.any_instance.expects(:to_otf)
+      FontAdapter.any_instance.expects(:to_woff)
+      FontAdapter.any_instance.expects(:to_eot)
+      FontAdapter.any_instance.expects(:to_svg)
+      ActionController::TestUploadedFile.expects(:new).times(5)
+      Format.any_instance.expects(:save!).times(5)
+      FileUtils.expects(:rm).times(5)
+      font = Factory.create(:font)
+    end
   end
 end
