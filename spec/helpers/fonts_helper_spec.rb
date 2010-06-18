0e4f1578bef3d3f845c93eef302fad580dc92b7d
John Grimes <me@johngrimes.info>
2010-06-18 11:18:11 +1000

    Adding to FontsHelper spec

--- spec/helpers/fonts_helper_spec.rb
+++ spec/helpers/fonts_helper_spec.rb
@@ -3,16 +3,60 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe FontsHelper do
   include FontsHelper
 
-  it 'should generate include code for font with all three formats' do
-    font = Factory.build(:font)
-    font.stubs(:id).returns(12345)
-    font.stubs(:font_family).returns('Duality')
-    font.stubs(:font_subfamily).returns('Regular')
-    font.expects(:format).with(:svg, :raise_error => false).returns(true)
-    font.expects(:format).with(:eot, :raise_error => false).returns(true)
-    font.expects(:format).with(:woff, :raise_error => false).returns(true)
-    font.expects(:format).with(:otf, :raise_error => false).returns(true)
-    font.expects(:format).with(:ttf, :raise_error => false).returns(true)
-    include_code(font, :unique_font_names => true, :include_markup => true)
+  before do
+    @font = Factory.build(:font)
+    @font.stubs(:id).returns(12345)
+    @font.stubs(:font_family).returns('Duality')
+    @font.stubs(:font_subfamily).returns('Regular')
+    @font.expects(:format).with(:svg, :raise_error => false).returns(true)
+    @font.expects(:format).with(:eot, :raise_error => false).returns(true)
+    @font.expects(:format).with(:woff, :raise_error => false).returns(true)
+    @font.expects(:format).with(:otf, :raise_error => false).returns(true)
+    @font.expects(:format).with(:ttf, :raise_error => false).returns(true)
+  end
+
+  it 'should generate include code' do
+    result = include_code(@font)
+    result.should == '@font-face {
+  font-family: "Duality";
+  src: url("http://staging.typefront.com/fonts/12345.eot");
+  src: local("☺"),
+       url("http://staging.typefront.com/fonts/12345.woff") format("woff"),
+       url("http://staging.typefront.com/fonts/12345.ttf") format("truetype"),
+       url("http://staging.typefront.com/fonts/12345.otf") format("opentype"),
+       url("http://staging.typefront.com/fonts/12345.svg") format("svg");
+  font-weight: normal;
+  font-style: normal;
+}'
+  end
+
+  it 'should generate include code with unique font names' do
+    result = include_code(@font, :unique_font_names => true)
+    result.should == '@font-face {
+  font-family: "Duality 12345";
+  src: url("http://staging.typefront.com/fonts/12345.eot");
+  src: local("☺"),
+       url("http://staging.typefront.com/fonts/12345.woff") format("woff"),
+       url("http://staging.typefront.com/fonts/12345.ttf") format("truetype"),
+       url("http://staging.typefront.com/fonts/12345.otf") format("opentype"),
+       url("http://staging.typefront.com/fonts/12345.svg") format("svg");
+  font-weight: normal;
+  font-style: normal;
+}'
+  end
+
+  it 'should generate include code with markup' do
+    result = include_code(@font, :include_markup => true)
+    result.should == '@font-face {
+  font-family: "Duality";
+<span class="eot-code">  src: url("http://staging.typefront.com/fonts/12345.eot");
+</span><span class="noneot-code">  src: local("☺"),
+       <span class="woff-code">url("http://staging.typefront.com/fonts/12345.woff") format("woff")</span><span class="woff-ttf-separator">,
+       </span><span class="ttf-code">url("http://staging.typefront.com/fonts/12345.ttf") format("truetype")</span><span class="ttf-otf-separator">,
+       </span><span class="otf-code">url("http://staging.typefront.com/fonts/12345.otf") format("opentype")</span><span class="otf-svg-separator">,
+       </span><span class="svg-code">url("http://staging.typefront.com/fonts/12345.svg") format("svg")</span>;
+</span><span class="style-descriptors">  font-weight: normal;
+  font-style: normal;
+</span>}'
   end
 end
