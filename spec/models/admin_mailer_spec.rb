2ee976ff402cf73c25291a187cd7deb2287400b3
John Grimes <me@johngrimes.info>
2010-06-18 12:48:33 +1000

    Refactoring spec for admin mailer

--- spec/models/admin_mailer_spec.rb
+++ spec/models/admin_mailer_spec.rb
@@ -1,35 +1,33 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe AdminMailer do
-  fixtures :all
-
-  it "should deliver new user joined email" do
-    doing {
+  describe 'new_user_joined' do
+    it 'should be successful' do
       AdminMailer.deliver_new_user_joined users(:bob)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 
-  it "should deliver payment received email" do
-    doing {
+  describe 'payment_received' do
+    it 'should be successful' do
       AdminMailer.deliver_payment_received invoices(:success)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
-  
-  it "should deliver payment failed email" do
-    doing {
+
+  describe 'payment_failed' do
+    it 'should be successful' do
       AdminMailer.deliver_payment_failed invoices(:failure)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 
-  it "should deliver billing job error email" do
-    doing {
+  describe 'billing_job_error' do
+    it 'should be successful' do
       AdminMailer.deliver_billing_job_error users(:bob), 'Some error message'
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 
-  it "should deliver billing job missed window email" do
-    doing {
+  describe 'billing_job_missed_window' do
+    it 'should be successful' do
       AdminMailer.deliver_billing_job_missed_window users(:bob)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 end
