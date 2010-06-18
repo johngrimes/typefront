b8a73f7a5b13620147008196dd739549eadbafe0
John Grimes <me@johngrimes.info>
2010-06-18 12:58:58 +1000

    Refactoring spec for user mailer

--- spec/models/user_mailer_spec.rb
+++ spec/models/user_mailer_spec.rb
@@ -1,23 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe UserMailer do
-  fixtures :all
-
-  it "should deliver activation email" do
-    doing {
+  describe 'activation' do
+    it 'should be successful' do
       UserMailer.deliver_activation users(:bob)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 
-  it "should deliver receipt email" do
-    doing {
+  describe 'receipt' do
+    it 'should be successful' do
       UserMailer.deliver_receipt invoices(:success)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 
-  it "should deliver password reset email" do
-    doing {
+  describe 'password_reset' do
+    it 'should be successful' do
       UserMailer.deliver_password_reset users(:john)
-    }.should change(ActionMailer::Base.deliveries, :size).by(+1)
+    end
   end
 end
