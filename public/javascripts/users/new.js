f5e7e03dd7c13233fda3bc92a417f19a7f56d98d
John Grimes <me@johngrimes.info>
2010-08-10 17:38:02 +1000

    Adding VOSS to new user form submission

--- public/javascripts/users/new.js
+++ public/javascripts/users/new.js
@@ -1,3 +1,9 @@
+var loadingCode = '<img class="loading" alt="Loading..." src="/images/loading.gif" width="16" height="11"/><span class="loading-message">Setting up your account...</span>';
+
 $(document).ready(function() {
   $('.accept-terms a').openInNewWindow();
+  $('#new-user-form').submit(function() {
+    $(this).find('.submit').replaceWith(loadingCode);
+    return true;
+  });
 });
