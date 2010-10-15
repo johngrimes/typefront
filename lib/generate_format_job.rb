e353215f17ea65a14cda4205ad5b1b46c188dc84
John Grimes <me@johngrimes.info>
2010-10-15 16:46:27 +1000

    Add failure hook for generate font format job

--- lib/generate_format_job.rb
+++ lib/generate_format_job.rb
@@ -5,7 +5,12 @@ class GenerateFormatJob
     options.symbolize_keys!
     font = Font.find(options[:font_id])
     font.generate_format(options[:format], options[:description])
-  ensure
+    font.update_attribute(:generate_jobs_pending, font.generate_jobs_pending - 1)
+  end
+
+  def self.on_failure_update_job_count(exception, *args)
+    options = args.last.symbolize_keys!
+    font = Font.find(options[:font_id])
     font.update_attribute(:generate_jobs_pending, font.generate_jobs_pending - 1)
   end
 end
