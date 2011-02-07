ab42b5ae163bd922f1fa73299fc3d60bff80bc4f
John Grimes <me@johngrimes.info>
2011-02-07 20:42:52 +1000

    Encoding logged raw requests as base64

--- app/models/logged_request.rb
+++ app/models/logged_request.rb
@@ -1,4 +1,12 @@
 class LoggedRequest < ActiveRecord::Base
   belongs_to :user
   belongs_to :font
+
+  def raw_request=(request_text)
+    self.write_attribute(:raw_request, Base64.encode64(request_text)) unless request_text.blank?
+  end
+
+  def raw_request
+    Base64.decode64(self.read_attribute(:raw_request)) unless self.read_attribute(:raw_request).blank?
+  end
 end
