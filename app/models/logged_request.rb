56f46851c1fa95053df1cf44b9e0a51fdf732f08
John Grimes <me@johngrimes.info>
2011-02-07 20:58:44 +1000

    Encoding logged user agent as base64

--- app/models/logged_request.rb
+++ app/models/logged_request.rb
@@ -2,6 +2,14 @@ class LoggedRequest < ActiveRecord::Base
   belongs_to :user
   belongs_to :font
 
+  def user_agent=(user_agent_text)
+    self.write_attribute(:user_agent, Base64.encode64(user_agent_text)) unless user_agent_text.blank?
+  end
+
+  def user_agent
+    Base64.decode64(self.read_attribute(:user_agent)) unless self.read_attribute(:user_agent).blank?
+  end
+
   def raw_request=(request_text)
     self.write_attribute(:raw_request, Base64.encode64(request_text)) unless request_text.blank?
   end
