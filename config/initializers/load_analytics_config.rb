946fb917f723090e6c89981f850206fe27c71812
John Grimes <john@smallspark.com.au>
2013-07-01 15:12:40 +1000

    Prevent deletion of key from analytics config constant

--- config/initializers/load_analytics_config.rb
+++ config/initializers/load_analytics_config.rb
@@ -2,7 +2,7 @@ raw_config = File.read(Rails.root.to_s + '/config/analytics_config.yml')
 ANALYTICS_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
 
 require 'km'
-options = ANALYTICS_CONFIG
+options = ANALYTICS_CONFIG.dup
 key = options.delete(:key)
 options.merge!(:log_dir => File.join(RAILS_ROOT, 'log'))
 KM.init(key, options)
