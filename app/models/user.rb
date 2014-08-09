0310df8aef22d19d26b51f6632dd92ed60116b20
John Grimes <john@smallspark.com.au>
2014-08-09 19:27:57 +1000

    Change prices on Plus and Power plans

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,8 @@ class User < ActiveRecord::Base
   POWER = 2
 
   PLANS = [ { :name => 'Free', :amount => 0, :fonts_allowed => 1, :requests_allowed => 500 },
-            { :name => 'Plus', :amount => 5, :fonts_allowed => 10, :requests_allowed => 5000 },
-            { :name => 'Power', :amount => 15, :fonts_allowed => 100000, :requests_allowed => 20000 },
+            { :name => 'Plus', :amount => 15, :fonts_allowed => 10, :requests_allowed => 5000 },
+            { :name => 'Power', :amount => 45, :fonts_allowed => 100000, :requests_allowed => 20000 },
             { :name => 'Custom', :amount => 95, :fonts_allowed => 100000, :requests_allowed => 200000 } ]
 
   SUPPORTED_CARDS = { :visa => 'Visa',
